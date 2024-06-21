bb5dcce28ce59f74c4f549ccb1704b6512713457
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-21 16:28:43 +0530

    add react redux store config

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
+import { Provider } from "react-redux";
 import Main from "./Main";
+import { store } from "./redux/store";
 
 const App = () => {
-  return <Main />;
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
 };
 
 export default App;
