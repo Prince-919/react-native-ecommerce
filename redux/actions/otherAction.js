0872b979fcf49fbc094640c29cf332904bb7a544
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-23 12:53:55 +0530

    craete place order reducer and action

--- redux/actions/otherAction.js
+++ redux/actions/otherAction.js
@@ -94,3 +94,52 @@ export const updatePicture = (formData) => async (dispatch) => {
     });
   }
 };
+
+export const placeOrder =
+  (
+    orderItems,
+    shippingInfo,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingCharges,
+    totalAmount,
+    paymentInfo
+  ) =>
+  async (dispatch) => {
+    try {
+      dispatch({
+        type: "placeOrderRequest",
+      });
+
+      const { data } = await axios.post(
+        `${server}/order/new`,
+        {
+          orderItems,
+          shippingInfo,
+          paymentMethod,
+          itemsPrice,
+          taxPrice,
+          shippingCharges,
+          totalAmount,
+          paymentInfo,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      dispatch({
+        type: "placeOrderSuccess",
+        payload: data.message,
+      });
+    } catch (error) {
+      dispatch({
+        type: "placeOrderFail",
+        payload: error.response.data.message,
+      });
+    }
+  };
